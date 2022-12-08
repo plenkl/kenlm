8a22611e0980598132056a5a33d9016e1174cda9
Erik Larsson <25484205+plenkl@users.noreply.github.com>
2022-12-08 18:22:06 +0100

    Test

--- rskenlm/build.rs
+++ rskenlm/build.rs
@@ -24,7 +24,7 @@ fn main() {
         .clang_arg("-x")
         .clang_arg("c++")
         .clang_arg("-std=c++11")
-        .clang_arg("-g")
+        .clang_arg("-lboost_system")
         .clang_arg("-DKENLM_MAX_ORDER=6")
         .clang_arg("-D_GLIBCXX_USE_CXX11_ABI=0")
         .enable_cxx_namespaces()
